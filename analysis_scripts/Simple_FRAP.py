ce8c8a59bf392bc4084d37caa6452fe199193b0d
Will Moore <will@lifesci.dundee.ac.uk>
2013-08-23 15:12:50 +0100

    Fixing headers in example scripts

--- analysis_scripts/Simple_FRAP.py
+++ analysis_scripts/Simple_FRAP.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-#
-# Copyright (C) 2011 University of Dundee & Open Microscopy Environment.
-#                    All Rights Reserved.
-# Use is subject to license terms supplied in LICENSE.txt
-#
-
 """
-FOR TRAINING PURPOSES ONLY!
+ analysis_scripts/Simple_FRAP.py
+
+-----------------------------------------------------------------------------
+  Copyright (C) 2013 University of Dundee. All rights reserved.
+
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+------------------------------------------------------------------------------
+
+Simply analyses the average pixel intensity for ellipses in a 
+movie and attempts to estimate the half-time of FRAP recovery
+and the percent recovery (mobile fraction).
+Needs ellipses drawn on all timepoints to be included in the 
+analysis, and needs at least 1 pre-bleach frame.
 """
 
 import omero
